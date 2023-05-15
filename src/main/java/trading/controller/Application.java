dd589bfa3a3b3a7903cdc9fa66c183c3dd3e15dc
Jaladhi Dave <jaladhi1992@gmail.com>
2023-05-15 09:29:24 +0200

    Logging feature available
    - Added logging for tracing purpose

--- src/main/java/trading/controller/Application.java
+++ src/main/java/trading/controller/Application.java
@@ -2,13 +2,16 @@ package trading.controller;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
  *The class provides actual implementation for handling signal
  */
 public class Application implements SignalHandler {
-		
+	
+	Logger logger = LoggerFactory.getLogger(Application.class);
 	private static Map<Integer, Runnable> signalMap;
 	private static int signalCapacity;
 	Signal signal = new Signal();
@@ -26,7 +29,7 @@ public class Application implements SignalHandler {
 	 *@param signal the signal number
 	 */
 	public void handleSignal(int signal) {
-		
+		logger.trace("Inside handleSignal method.");
 		if (signal > 0 && signal <= signalCapacity) {
 			signalMap.get(signal).run();
 			signalMap.get(signalCapacity + 2).run();
@@ -35,6 +38,6 @@ public class Application implements SignalHandler {
 			signalMap.get(signalCapacity + 2).run();
 		}
 		
-		
+		logger.trace("Finished executing handleSignal method.");
 	}
 }
